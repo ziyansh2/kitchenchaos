85a29b563052a3d6358a14374d7c175471222e97
bai <bai@skeletoncrew.co.jp>
2024-09-30 13:12:45 +0900

    [Add] spawn objects on a counter

--- KichenChaos/Assets/Scripts/ClearCounter.cs
+++ KichenChaos/Assets/Scripts/ClearCounter.cs
@@ -3,7 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
     public void Interact() {
-        Debug.Log("Interact " + gameObject.name);
+        if (kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        } else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;    
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
     }
 }
