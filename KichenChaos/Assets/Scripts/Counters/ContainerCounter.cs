233a10ccde7b12260476611450522b1d08b0d391
bai <bai@skeletoncrew.co.jp>
2024-10-20 20:48:18 +0900

    [Update] to sync the animation trigger on container counter

--- KichenChaos/Assets/Scripts/Counters/ContainerCounter.cs
+++ KichenChaos/Assets/Scripts/Counters/ContainerCounter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter {
@@ -13,8 +14,17 @@ public class ContainerCounter : BaseCounter {
         if (!player.HasKitchenObject()) {
             //Player is not carrying anything
             KitchenObject.SpawnKitchenObject(kitchenObjectSO, player);
-            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+            InteractLogicServerRpc();
         }
     }
 
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractLogicServerRpc() {
+        InteractLogicClientRpc();
+    }
+
+    [ClientRpc]
+    private void InteractLogicClientRpc() {
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+    }
 }
