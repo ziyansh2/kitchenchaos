39f3bf63a795acbf148f6d479b675593ca37a49c
bai <bai@skeletoncrew.co.jp>
2024-11-02 19:30:48 +0900

    [Add] button to trigger help view [Add] parameter info to the help view

--- KichenChaos/Assets/DebugConsole/Script/S_DebugConsoleHelpElement.cs
+++ KichenChaos/Assets/DebugConsole/Script/S_DebugConsoleHelpElement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using static DebugConsole.S_DebugConsoleInput;
 
@@ -11,12 +12,30 @@ namespace DebugConsole {
 		[SerializeField] TMPro.TextMeshProUGUI parametersText;
 		[SerializeField] TMPro.TextMeshProUGUI descriptionText;
 
-
+		static readonly Dictionary<string, string> parameterTypeMap = new () {
+			{ "System.String", "string" },
+			{ "System.Int32", "int" },
+			{ "System.Single", "float" },
+			{ "System.Boolean", "bool" }
+		};
 
 		public void SetMethodContents(string command, ActionInfoData actionInfoData) {
 			commandText.text = command;
 			descriptionText.text = actionInfoData.methodDescription;
-			parametersText.text = "";
+
+			ParameterInfo[] pars = actionInfoData.methodInfo.GetParameters();
+			string paramaterView = "";
+            for (int i = 0; i < pars.Length; i++) {
+				string typeName = pars[i].ParameterType.ToString();
+				if (parameterTypeMap.ContainsKey(typeName)){
+					typeName = parameterTypeMap[typeName];
+				}
+				paramaterView += $"{pars[i].Name} ({typeName})";
+				if (i != pars.Length - 1) {
+					paramaterView += "\n";
+				}
+			}
+            parametersText.text = paramaterView;
 		}
 	}
 
